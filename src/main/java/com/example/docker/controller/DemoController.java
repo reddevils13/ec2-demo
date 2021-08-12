61487ccb1d0b01da812a4ab2f4eca345afc14301
arimusta <aritramustafi56@gmail.com>
2021-08-12 18:30:47 +0530

    asdfghj

--- src/main/java/com/example/docker/controller/DemoController.java
+++ src/main/java/com/example/docker/controller/DemoController.java
@@ -10,14 +10,21 @@ public class DemoController {
 	@Value("${RDS_URL}")
 	private String RDS_URL;
 	
+	@Value("${RDS_PASSWORD}")
+	private String RDS_PASSWORD;
+	
+	@Value("${RDS_USERNAME}")
+	private String RDS_USERNAME;
+	
+	
 	@GetMapping("/")
 	public String home() {
 		return "Welcome to Docker World!";
 	}
 	
-	@GetMapping("/get")
+	@GetMapping("/var")
 	public String getEnv() {
-		return RDS_URL;
+		return RDS_PASSWORD+" "+RDS_URL+" "+RDS_USERNAME;
 	}
 	
 }
